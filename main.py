c25a017b5590693c1f6aeba3860d73b451534e9d
Rafael <rafaelhitachi@gmail.com>
2021-11-20 19:34:16 -0500

    Creación de ciclos para correcto control de las opciones del menú

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 
 def menuPrincipal():
-    print("...:::  MENÚ PRINCIPAL  :::...")
-    print("1. Listar cursos")
-    print("2. Registrar curso")
-    print("3. Actualizar curso")
-    print("4. Eliminar curso")
-    print("5. SALIR")
-    print("------------------------------")
+    continuar = True
+    while(continuar):
+        opcionCorrecta = False
+        while(not opcionCorrecta):
+            print("...:::  MENÚ PRINCIPAL  :::...")
+            print("1. Listar cursos")
+            print("2. Registrar curso")
+            print("3. Actualizar curso")
+            print("4. Eliminar curso")
+            print("5. SALIR")
+            print("------------------------------")
 
-    opcion = int(input("Seleccione una opción: "))
+            opcion = int(input("Seleccione una opción: "))
 
-    if opcion < 1 or opcion > 5:
-        print("Opción incorrecta, ingrese una opción correcta...")
-    elif opcion == 5:
-        print("¡Gracias por usar el sistema! Hasta la próxima")
-    else:
-        ejecutarOpcion(opcion)
+            if opcion < 1 or opcion > 5:
+                print("¡ERROR! Opción incorrecta, ingrese una opción correcta... \n")
+            elif opcion == 5:
+                continuar = False
+                print("¡Gracias por usar el sistema! Hasta la próxima \n")
+                break
+            else:
+                opcionCorrecta = True
+                ejecutarOpcion(opcion)
 
 
 def ejecutarOpcion(opcion):
